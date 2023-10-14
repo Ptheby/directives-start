3407827f6122c60be2a4864889d72510db77168f
Paul Theby <paultheby@gmail.com>
2023-10-14 13:19:51 -0500

    HostBinding!

--- src/app/better-highlight/better-higlight.directive.ts
+++ src/app/better-highlight/better-higlight.directive.ts
@@ -1,9 +1,16 @@
-import { Directive,Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
+import { Directive,
+  Renderer2,
+   OnInit,
+    ElementRef,
+    HostListener,
+    HostBinding
+  } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHiglight]'
 })
 export class BetterHiglightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor:string='pink'
 
 
   constructor(private elRef:ElementRef, private renderer: Renderer2) { }
@@ -12,10 +19,12 @@ export class BetterHiglightDirective implements OnInit {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue', );
   }
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue', );
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue', );
+    this.backgroundColor='blue';
   }
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent', );
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent', );
+    this.backgroundColor= 'transparent';
   }
 
 }
