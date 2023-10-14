5e463ee57c606de57bab28455afb8945202876c7
Paul Theby <paultheby@gmail.com>
2023-10-14 12:50:11 -0500

    Using HostListening

--- src/app/better-highlight/better-higlight.directive.ts
+++ src/app/better-highlight/better-higlight.directive.ts
@@ -1,13 +1,21 @@
-import { Directive,Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive,Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHiglight]'
 })
 export class BetterHiglightDirective implements OnInit {
 
+
   constructor(private elRef:ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue', );
+  }
+  @HostListener('mouseenter') mouseover(eventData: Event) {
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue', );
   }
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent', );
+  }
+
 }
